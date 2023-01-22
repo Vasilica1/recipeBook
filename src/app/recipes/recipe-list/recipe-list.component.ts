626e9a2c86edf2954ebbd33714a4bf5aaf5143ed
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-23 01:37:38 +0200

    Outpu the list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe("a test recipe", "this is just a test", "https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg")
+    new Recipe("a test recipe", "this is just a test", "https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg"),
+    new Recipe("clatite", "cu dulceata si nutella", "https://savoriurbane.com/wp-content/uploads/2019/10/Cl%C4%83tite-pufoase-%C8%99i-fragede-cea-mai-simpl%C4%83-re%C8%9Bet%C4%83-care-nu-d%C4%83-gre%C8%99-savori-urbane.jpg")
   ];
 
 
