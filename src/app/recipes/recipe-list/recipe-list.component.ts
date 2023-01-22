0132b25b45d2814cfdfbc42f9c59f4e1d2204c7e
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-23 01:25:57 +0200

    add content to the recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  
+  recipes: Recipe[] = [
+    new Recipe("a test recipe", "this is just a test", "https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg")
+  ];
+
+
 }
