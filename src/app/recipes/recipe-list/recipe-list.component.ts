53b7a9d2e2936ba412a62782416309eafb128190
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-27 03:46:01 +0200

    Passing Data with Event and Propery Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,6 +12,10 @@ export class RecipeListComponent {
     new Recipe("a test recipe", "this is just a test", "https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg"),
     new Recipe("clatite", "cu dulceata si nutella", "https://savoriurbane.com/wp-content/uploads/2019/10/Cl%C4%83tite-pufoase-%C8%99i-fragede-cea-mai-simpl%C4%83-re%C8%9Bet%C4%83-care-nu-d%C4%83-gre%C8%99-savori-urbane.jpg")
   ];
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
+  onRecipeSelected(recipeItem: Recipe) {
+    this.recipeWasSelected.emit(recipeItem);
+  }
 
 }
